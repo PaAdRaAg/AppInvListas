632e42a272528b154102281bdd4b0c5921389cac
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-11-08 00:53:48 -0600

    dos

--- invListApp.js
+++ invListApp.js
@@ -1,3 +1,21 @@
+var cantidad = document.querySelector("#cantPA")
+var codigo = document.querySelector("#codPA")
+var nombre = document.querySelector("#nomPA")
+var costo = document.querySelector("#costPA")
+var descripcion = document.querySelector("#descPA")
+var btnAg = document.querySelector("#agregarBtn")
+var tabla1 = document.querySelector("#tabla1")
+var tabla2 = document.querySelector("#tabla2")
+var tabla3 = document.querySelector("#tabla3")
+var tabla4 = document.querySelector("#tabla4")
+var codigoEl = document.querySelector("#codPE")
+var btnEl = document.querySelector("#btnPE")
+var codBu = document.querySelector("#codPB")
+var btnBu = document.querySelector("#btnPB")
+var btnListar = document.querySelector("#btnListar")
+var btnListarInv = document.querySelector("#btnListarInv")
+var btnAgPos = document.querySelector("#btnAgPos")
+
 class Producto{
     constructor(cantidad, codigo, nombre, costo, descripcion){
         this.cantidad = cantidad;
@@ -13,6 +31,7 @@ class Inventario{
     constructor(){
         this.inicio = null;
         this.producto = new Producto();
+        this.tamaño = 0;
 
     };
 
@@ -27,6 +46,7 @@ class Inventario{
             };
             aux.siguiente = nProducto;
         };
+        this.tamaño++;
     };
 
     eliminar(codigo){
@@ -40,6 +60,7 @@ class Inventario{
                 } else{
                     anterior.siguiente = aux.siguiente;
                 };
+                this.tamaño--;
                 return aux.codigo;
             };
             anterior = aux;
@@ -48,35 +69,54 @@ class Inventario{
         return null;
     };
 
-    buscar(cod){
+    buscar(codigo){
+        if(this.inicio == null){
+            return null;
+        };
         let aux = this.inicio;
-        let anterior = null;
-
-        while(aux != null){
-            if(aux.codigo === cod){
-                if(!anterior){
-                    this.inicio = aux.siguiente;
-                } else{
-                    anterior.siguiente = aux.siguiente;
-                };
+        while(aux){
+            if(aux.codigo == codigo){
                 return aux;
             };
+            aux = aux.siguiente;
         };
+        return null;
+    };
+
+    insertar(producto, inicio){
+        if(inicio < 0 || inicio > this.tamaño){
+            return null;
+        };
+
+        const nProducto = new Node(producto);
+        let aux = this.inicio;
+        let anterior;
+
+        if(inicio === 0){
+            nProducto.siguiente = aux;
+            this.inicio = nProducto;
+        } else {
+            for(let i = 0; i < inicio; i++){
+                anterior = aux;
+                aux = aux.siguiente;
+            };
+
+            nProducto.siguiente = aux;
+            anterior.siguiente = nProducto;
+        };
+        this.tamaño++;
+    };
+
+    agregarIn(codigo, producto, cantidad, precio, descripcion){
+        const nProducto = new Producto(codigo, producto, cantidad, precio, descripcion);
+        let aux = this.inicio;
+        nProducto.siguiente = aux;
+        this.inicio = nProducto;
+    };
+
+    eliminarIn(){
+        let aux = this.inicio;
+        this.inicio = this.inicio.siguiente;
+        aux.siguiente = null;
     };
 };
-
-const nInventario = new Inventario();
-
-nInventario.agregar(1, 1, 1, 1, 1)
-nInventario.agregar(2, 2, 2, 2, 2)
-nInventario.agregar(3, 3, 3, 3, 3)
-
-console.log(nInventario);
-
-//nInventario.eliminar(2);
-
-//console.log(nInventario);
-
-//nInventario.buscar(3);
-
-//console.log(nInventario);
